82e3d546682b068ba7b324b787f56abf4a0066d4
Luiz-Felipe-Trindade <luiz.f.trindade0@gmail.com>
2025-05-05 15:32:59 -0300

    Criado page de login e router

---
+++ VuePOC/src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../view/Login/Login.vue";
+
+const routes = [{ path: "/login", name: "Login", component: Login }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ VuePOC/src/view/Login/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-container">
+    <h2>Login</h2>
+    <form @submit.prevent="handleLogin">
+      <input type="email" v-model="email" placeholder="Email" required />
+      <input type="password" v-model="password" placeholder="Senha" required />
+      <button type="submit">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const email = ref("");
+const password = ref("");
+const error = ref("");
+const router = useRouter();
+
+function handleLogin() {
+  router.push("/dashboard");
+}
+</script>
+
+<style scoped>
+.login-container {
+  height: 95vh;
+  margin: 0px auto;
+  justify-content: center;
+  align-items: center;
+
+  display: flex;
+  flex-direction: column;
+  font-family: "Roboto", sans-serif;
+}
+input {
+  margin-bottom: 10px;
+  padding: 8px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
