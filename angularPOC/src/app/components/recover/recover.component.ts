618089db105a188155bed479224c1d36e4c0ee1f
AmandaH <amanda.henz@unochapeco.edu.br>
2025-05-07 22:46:16 -0300

    criando componente de recover password

---
+++ angularPOC/src/app/components/recover/recover.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-recover',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './recover.component.html',
+  styleUrls: ['./recover.component.css'],
+})
+export class RecoverComponent {
+  email: string = '';
+
+  constructor(private router: Router) {}
+
+  recover() {
+    console.log('E-mail para recuperação:', this.email);
+
+    this.router.navigate(['/login']);
+  }
+}
+
