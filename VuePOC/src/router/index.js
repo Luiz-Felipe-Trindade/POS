0a4dd13824580e4b42ba418c7d1b900428e12bb4
gabi.becker <g.becker@unochapeco.edu.br>
2025-05-07 23:15:52 -0300

    Feita a pagina Home em Vue.js

--- VuePOC/src/router/index.js
+++ VuePOC/src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../view/Login/Login.vue";
+import Home from "../view/Home/HomePage.vue"
 
-const routes = [{ path: "/login", name: "Login", component: Login }];
+const routes = [
+  { patch: "/", name: "Home", component: Home},
+  { path: "/login", name: "Login", component: Login }];
 
 const router = createRouter({
   history: createWebHistory(),
