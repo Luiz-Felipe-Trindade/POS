1f2e562861053e1eb15a93bc7eaa39ef6f5b6ac1
Luiz-Felipe-Trindade <luiz.f.trindade0@gmail.com>
2025-05-08 14:10:11 -0300

    Ajuste final Vue

--- VuePOC/src/router/index.js
+++ VuePOC/src/router/index.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../view/Login/Login.vue";
-import Home from "../view/Home/HomePage.vue"
-import Cadastro from "../view/Cadastro/Cadastro.vue"
+import Home from "../view/Home/HomePage.vue";
+import Cadastro from "../view/Cadastro/Cadastro.vue";
+import Recovery from "../view/Recovery/Recovery.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/login", name: "Login", component: Login },
-  { path: "/cadastro", name: "Cadastro", component: Cadastro }
+  { path: "/cadastro", name: "Cadastro", component: Cadastro },
+  { path: "/recovery", component: Recovery },
 ];
-
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ VuePOC/src/view/Recovery/Recovery.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="recover-container">
+    <div class="recover-card">
+      <h2>Recuperação de Senha</h2>
+      <p class="subtitle">
+        Digite seu e-mail para receber um link de recuperação de senha
+      </p>
+      <form @submit.prevent="handleRecover">
+        <label for="email">E-mail</label>
+        <input
+          id="email"
+          type="email"
+          v-model="email"
+          placeholder="seu@email.com"
+          required
+        />
+        <button type="submit">Enviar link de recuperação</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+
+function handleRecover() {
+  alert("Link de recuperação enviado para: " + email.value);
+}
+</script>
+
+<style scoped>
+.recover-container {
+  background-color: #e5e5e5;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.recover-card {
+  background-color: #ffffff;
+  padding: 30px;
+  border-radius: 10px;
+  width: 100%;
+  max-width: 450px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  font-family: "Roboto", sans-serif;
+  text-align: center;
+}
+
+h2 {
+  margin-bottom: 10px;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.subtitle {
+  font-size: 14px;
+  color: #666;
+  margin-bottom: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  text-align: left;
+}
+
+label {
+  font-size: 14px;
+  font-weight: 600;
+}
+
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+
+button {
+  padding: 10px;
+  background-color: #121212;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #2c2c2c;
+}
+</style>
