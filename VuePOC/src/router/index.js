16372be12da7dd7b3518739f78be2cb4656c83b5
Vitor Squina <vitor122703@gmail.com>
2025-05-08 01:47:41 -0300

    Criando cadastro Empresa

--- VuePOC/src/router/index.js
+++ VuePOC/src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../view/Login/Login.vue";
 import Home from "../view/Home/HomePage.vue"
+import Cadastro from "../view/Cadastro/Cadastro.vue"
 
 const routes = [
-  { patch: "/", name: "Home", component: Home},
-  { path: "/login", name: "Login", component: Login }];
+  { path: "/", name: "Home", component: Home },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/cadastro", name: "Cadastro", component: Cadastro }
+];
+
 
 const router = createRouter({
   history: createWebHistory(),
--- VuePOC/src/view/Cadastro/Cadastro.vue
+++ VuePOC/src/view/Cadastro/Cadastro.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="login-container">
-    <h2>Login Empresa</h2>
-    <form @submit.prevent="handleLogin">
-      <input type="email" v-model="email" placeholder="Email" required />
-      <input type="password" v-model="password" placeholder="Senha" required />
+  <div class="cadastro-container">
+    <h2>Cadastro Empresa</h2>
+    <form @submit.prevent="handleCadastro">
+      <input  v-model="cnpj" placeholder="CNPJ" required />
+      <input v-model="nome" placeholder="Nome Fantasia" required />
       <button type="submit">Entrar</button>
     </form>
   </div>
@@ -13,18 +13,18 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
-const email = ref("");
-const password = ref("");
+const cnpj = ref("");
+const nome = ref("");
 const error = ref("");
 const router = useRouter();
 
-function handleLogin() {
+function handleCadastro() {
   router.push("/dashboard");
 }
 </script>
 
 <style scoped>
-.login-container {
+.cadastro-container {
   height: 55vh;
   margin: 0px auto;
   justify-content: center;
@@ -36,9 +36,9 @@ function handleLogin() {
 }
 input {
   border-radius: 10px;
+  border: 1px solid;
   width: 300px;
   height: 20px;
-  border: 1px solid;
   margin-bottom: 10px;
   padding: 8px;
 }
@@ -47,10 +47,10 @@ form {
   flex-direction: column;
 }
 button {
+    width: 320px;
+  height: 30px;
   border: 1px solid;
   border-radius: 10px;
   cursor: pointer;
-  width: 320px;
-  height: 30px;
 }
 </style>
