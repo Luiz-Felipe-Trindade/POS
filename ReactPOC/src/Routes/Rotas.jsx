9fd3385d379118687ed8bafea7a99c521e9ec077
amandahenz <amanda.henz@unochapeco.edu.br>
2025-05-07 23:40:51 -0300

    codigo react

--- ReactPOC/src/Routes/Rotas.jsx
+++ ReactPOC/src/Routes/Rotas.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../components/Login/Login";
+import { RecoverPassword} from "../components/RecoverPassword/RecoverPassword";
 
 export const Rotas = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/RecoverPassword" element={<RecoverPassword />}></Route>
       </Routes>
     </BrowserRouter>
   );
